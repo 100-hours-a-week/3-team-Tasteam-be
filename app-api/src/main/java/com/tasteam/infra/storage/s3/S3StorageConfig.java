a03299e571cfe6d38b9bcdc0f9a993fbd52158e8
SH Woo <imdlsrks.mc@gmail.com>
2026-01-29 14:43:37 +0900

    ✨ feat: AWS storage 구현체 추가

--- app-api/src/main/java/com/tasteam/infra/storage/s3/S3StorageConfig.java
+++ app-api/src/main/java/com/tasteam/infra/storage/s3/S3StorageConfig.java
@@ -6,8 +6,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.util.Assert;
 
+import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.tasteam.infra.storage.StorageProperties;
@@ -18,17 +20,21 @@ import com.tasteam.infra.storage.StorageProperties;
 public class S3StorageConfig {
 
 	@Bean
-	public AmazonS3 amazonS3(StorageProperties properties) {
+	public AWSCredentialsProvider awsCredentialsProvider(StorageProperties properties) {
+		if (properties.hasStaticCredentials()) {
+			return new AWSStaticCredentialsProvider(
+				new BasicAWSCredentials(properties.getAccessKey(), properties.getSecretKey()));
+		}
+		return DefaultAWSCredentialsProviderChain.getInstance();
+	}
+
+	@Bean
+	public AmazonS3 amazonS3(StorageProperties properties, AWSCredentialsProvider credentialsProvider) {
 		Assert.hasText(properties.getRegion(), "tasteam.storage.region은 필수입니다");
 
-		AmazonS3ClientBuilder builder = AmazonS3ClientBuilder.standard()
-			.withRegion(properties.getRegion());
-
-		if (properties.hasStaticCredentials()) {
-			builder.withCredentials(new AWSStaticCredentialsProvider(
-				new BasicAWSCredentials(properties.getAccessKey(), properties.getSecretKey())));
-		}
-
-		return builder.build();
+		return AmazonS3ClientBuilder.standard()
+			.withRegion(properties.getRegion())
+			.withCredentials(credentialsProvider)
+			.build();
 	}
 }
