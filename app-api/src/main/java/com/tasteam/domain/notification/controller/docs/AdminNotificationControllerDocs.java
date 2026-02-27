08a011fa72f1634262305951fa327012772fa28d
SH Woo <imdlsrks.mc@gmail.com>
2026-02-28 00:43:52 +0900

    ✨ feat(notification): 어드민 브로드캐스트 엔드포인트 추가
    POST /push/broadcast, POST /email/broadcast 엔드포인트 및 Swagger 문서 추가

--- app-api/src/main/java/com/tasteam/domain/notification/controller/docs/AdminNotificationControllerDocs.java
+++ app-api/src/main/java/com/tasteam/domain/notification/controller/docs/AdminNotificationControllerDocs.java
@@ -3,7 +3,10 @@ package com.tasteam.domain.notification.controller.docs;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import com.tasteam.domain.notification.dto.request.AdminBroadcastEmailRequest;
+import com.tasteam.domain.notification.dto.request.AdminBroadcastPushRequest;
 import com.tasteam.domain.notification.dto.request.AdminPushNotificationRequest;
+import com.tasteam.domain.notification.dto.response.AdminBroadcastResultResponse;
 import com.tasteam.domain.notification.dto.response.AdminPushNotificationResponse;
 import com.tasteam.global.dto.api.SuccessResponse;
 import com.tasteam.global.swagger.annotation.SwaggerTagOrder;
@@ -20,4 +23,14 @@ public interface AdminNotificationControllerDocs {
 	ResponseEntity<SuccessResponse<AdminPushNotificationResponse>> sendTestPush(
 		@RequestBody @Valid
 		AdminPushNotificationRequest request);
+
+	@Operation(summary = "푸시 알림 브로드캐스트", description = "지정한 NotificationType의 PUSH 채널을 허용한 모든 회원에게 FCM 알림을 일괄 발송합니다.")
+	ResponseEntity<SuccessResponse<AdminBroadcastResultResponse>> broadcastPush(
+		@RequestBody @Valid
+		AdminBroadcastPushRequest request);
+
+	@Operation(summary = "이메일 일괄 발송", description = "지정한 NotificationType의 EMAIL 채널을 허용한 모든 회원에게 템플릿 이메일을 일괄 발송합니다.")
+	ResponseEntity<SuccessResponse<AdminBroadcastResultResponse>> broadcastEmail(
+		@RequestBody @Valid
+		AdminBroadcastEmailRequest request);
 }
