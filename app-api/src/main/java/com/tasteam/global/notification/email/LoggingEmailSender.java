e487ad4253999074faf8a426f9b5e3de44abfaf0
SH Woo <imdlsrks.mc@gmail.com>
2026-02-28 01:24:05 +0900

    ✨ feat: 이벤트 publisher 추가

--- app-api/src/main/java/com/tasteam/global/notification/email/LoggingEmailSender.java
+++ app-api/src/main/java/com/tasteam/global/notification/email/LoggingEmailSender.java
@@ -3,12 +3,14 @@ package com.tasteam.global.notification.email;
 import java.time.Instant;
 import java.util.Map;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Component
+@ConditionalOnProperty(prefix = "tasteam.mail", name = "enabled", havingValue = "false", matchIfMissing = true)
 public class LoggingEmailSender implements EmailSender {
 
 	@Override
