555ff6a9ff0d5acddba42a200a029e43c3c12cc8
SH Woo <imdlsrks.mc@gmail.com>
2026-02-28 16:18:09 +0900

    Revert "🐛 fix(subgroup): Subgroup ID를 SEQUENCE에서 IDENTITY 전략으로 변경"
    This reverts commit b1a0e3be41a45bb2aa4cb46f8b59f8632356a75c.

--- app-api/src/main/java/com/tasteam/domain/subgroup/entity/Subgroup.java
+++ app-api/src/main/java/com/tasteam/domain/subgroup/entity/Subgroup.java
@@ -17,6 +17,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -32,7 +33,8 @@ import lombok.NoArgsConstructor;
 @Table(name = "subgroup")
 public class Subgroup extends BaseTimeEntity {
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "subgroup_seq_gen")
+	@SequenceGenerator(name = "subgroup_seq_gen", sequenceName = "subgroup_seq", allocationSize = 50)
 	@Column(name = "id", nullable = false)
 	private Long id;
 
