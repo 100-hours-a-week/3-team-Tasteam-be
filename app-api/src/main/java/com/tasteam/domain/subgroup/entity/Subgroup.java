b1a0e3be41a45bb2aa4cb46f8b59f8632356a75c
SH Woo <imdlsrks.mc@gmail.com>
2026-02-28 16:10:27 +0900

    🐛 fix(subgroup): Subgroup ID를 SEQUENCE에서 IDENTITY 전략으로 변경
    pooled 옵티마이저가 START=1, INCREMENT=50 시퀀스의 첫 풀을 [-48, 1]로
    계산하여 음수 ID가 발생, DomainImage.validateCreate() 예외를 유발함.
    GenerationType.IDENTITY로 전환하여 항상 양수 ID를 보장하고
    코드베이스 컨벤션에도 부합시킴.

--- app-api/src/main/java/com/tasteam/domain/subgroup/entity/Subgroup.java
+++ app-api/src/main/java/com/tasteam/domain/subgroup/entity/Subgroup.java
@@ -17,7 +17,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -33,8 +32,7 @@ import lombok.NoArgsConstructor;
 @Table(name = "subgroup")
 public class Subgroup extends BaseTimeEntity {
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "subgroup_seq_gen")
-	@SequenceGenerator(name = "subgroup_seq_gen", sequenceName = "subgroup_seq", allocationSize = 50)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id", nullable = false)
 	private Long id;
 
