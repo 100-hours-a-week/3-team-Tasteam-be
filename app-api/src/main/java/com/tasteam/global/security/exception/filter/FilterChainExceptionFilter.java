706f10cc4bf7b30c0f196f426dea7198417df54b
SH Woo <imdlsrks.mc@gmail.com>
2026-03-02 14:50:10 +0900

    🐛 fix: 필터 예외 로그 사용자 입력 sanitize 처리

--- app-api/src/main/java/com/tasteam/global/security/exception/filter/FilterChainExceptionFilter.java
+++ app-api/src/main/java/com/tasteam/global/security/exception/filter/FilterChainExceptionFilter.java
@@ -26,6 +26,16 @@ public class FilterChainExceptionFilter extends OncePerRequestFilter {
 
 	private final SecurityResponseSender securityResponseSender;
 
+	/**
+	 * 사용자 입력을 로그에 넣기 전에 개행 문자를 제거해 로그 인젝션을 방지한다.
+	 */
+	private String sanitizeForLog(String value) {
+		if (value == null) {
+			return null;
+		}
+		return value.replace('\r', ' ').replace('\n', ' ');
+	}
+
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
 		FilterChain filterChain) throws ServletException, IOException {
@@ -34,8 +44,10 @@ public class FilterChainExceptionFilter extends OncePerRequestFilter {
 		} catch (AuthenticationException | AccessDeniedException e) {
 			throw e;
 		} catch (Exception e) {
-			log.error("FilterChain exception. requestId={}, method={}, uri={}", request.getHeader("X-Request-Id"),
-				request.getMethod(), request.getRequestURI(), e);
+			String requestId = sanitizeForLog(request.getHeader("X-Request-Id"));
+			String method = sanitizeForLog(request.getMethod());
+			String uri = sanitizeForLog(request.getRequestURI());
+			log.error("FilterChain exception. requestId={}, method={}, uri={}", requestId, method, uri, e);
 			securityResponseSender.sendError(response, HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
 				"서버 오류가 발생했습니다");
 		}
