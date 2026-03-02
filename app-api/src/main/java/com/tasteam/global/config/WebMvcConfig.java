9383d2737172135099b356225d0840fad39823d4
SH Woo <imdlsrks.mc@gmail.com>
2026-03-02 12:25:45 +0900

    🐛 fix(config): admin 정적 리소스 핸들러 통합 및 루트 리다이렉트 추가

--- app-api/src/main/java/com/tasteam/global/config/WebMvcConfig.java
+++ app-api/src/main/java/com/tasteam/global/config/WebMvcConfig.java
@@ -2,6 +2,7 @@ package com.tasteam.global.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -9,14 +10,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/admin/js/**")
+		registry.addResourceHandler("/admin/**")
 			.addResourceLocations("classpath:/static/admin/")
 			.setCachePeriod(0);
-		registry.addResourceHandler("/admin/css/**")
-			.addResourceLocations("classpath:/static/admin/")
-			.setCachePeriod(0);
-		registry.addResourceHandler("/admin/index.html")
-			.addResourceLocations("classpath:/static/admin/")
-			.setCachePeriod(0);
+	}
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addRedirectViewController("/admin", "/admin/index.html");
 	}
 }
