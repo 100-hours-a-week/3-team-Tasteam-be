a6c722b327fc4c68f9423e7928f2f3af261be8c9
SH Woo <imdlsrks.mc@gmail.com>
2026-03-14 23:55:41 +0900

    ♻️ refactor(policy): EXPANDED_RADII 제거, SECTION_RADIUS_METER=5000 고정 반경으로 단순화

--- app-api/src/main/java/com/tasteam/domain/restaurant/policy/RestaurantSearchPolicy.java
+++ app-api/src/main/java/com/tasteam/domain/restaurant/policy/RestaurantSearchPolicy.java
@@ -12,5 +12,5 @@ public final class RestaurantSearchPolicy {
 	public static final int MAX_PAGE_SIZE = 20;
 
 	public static final int SECTION_SIZE = 20;
-	public static final int[] EXPANDED_RADII = {3_000, 5_000, 10_000, 20_000};
+	public static final int SECTION_RADIUS_METER = 5_000;
 }
