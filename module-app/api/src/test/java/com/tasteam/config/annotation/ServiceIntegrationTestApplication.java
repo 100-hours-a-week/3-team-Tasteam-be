34a0a7cd81db1c620df80e9b20f96ab040e3002e
SH Woo <imdlsrks.mc@gmail.com>
2026-03-21 18:06:57 +0900

    refactor(test): 서비스 통합 테스트 경량 컨텍스트 분리 (#655)

---
+++ module-app/api/src/test/java/com/tasteam/config/annotation/ServiceIntegrationTestApplication.java
@@ -0,0 +1,6 @@
+package com.tasteam.config.annotation;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.tasteam.domain")
+public class ServiceIntegrationTestApplication {}
